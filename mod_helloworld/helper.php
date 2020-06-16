af0c5be53d688beec38ab13dfd0b7a1f5c0eb4cb
Drew Jaqua <anjaqua@gmail.com>
2020-06-16 10:53:48 -0400

    (midna) fixed whitespace in helper.php

--- mod_helloworld/helper.php
+++ mod_helloworld/helper.php
@@ -27,13 +27,13 @@ class ModHelloWorldHelper
 
     public static function doGet($url, $config)
     {
-		$ch = curl_init($url);
+        $ch = curl_init($url);
 
         // causes curl to return the transfer as a string of the return
         // value of curl_exec(...) instead of outputting it directly
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-		// curl_setopt($ch, CURLOPT_POST, 0);
+        // curl_setopt($ch, CURLOPT_POST, 0);
 
         // causes curl to follow any 'Location:' header that the server sends
         // as part of the HTTP header; WARNING: could potentially redirect
@@ -41,15 +41,15 @@ class ModHelloWorldHelper
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, $config->get('curl_follow_redirects', 0));
 
         // causes curl to skip verifying the peer's certificate
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, $config->get('curl_ssl_verify_peer', 1));
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, $config->get('curl_ssl_verify_peer', 1));
 
         // causes curl to skip checking for a common name in the peer certificate
-		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, $config->get('curl_ssl_verify_host', 2));
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, $config->get('curl_ssl_verify_host', 2));
 
         // finally, perform the GET request, close the connection, return the string
-		$data = curl_exec($ch);
-		curl_close($ch);
-		return $data;
+        $data = curl_exec($ch);
+        curl_close($ch);
+        return $data;
     }
 
 }
