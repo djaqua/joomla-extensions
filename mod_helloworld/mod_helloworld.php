48e05ce47b3510bede839ec781844d68768fc13c
Drew Jaqua <anjaqua@gmail.com>
2020-06-16 01:35:14 -0400

    (midna) added configurable module

---
+++ mod_helloworld/mod_helloworld.php
@@ -0,0 +1,15 @@
+<?php
+/***
+* @package Joomla.Site
+* @subpackage mod_firstmodule
+* @license GNU/GPL, see LICENSE.php
+* @copyright Copyright (C) 2005 - 2018, Open Source Matters, Inc. All rights reserved.
+***/
+// no direct access
+defined('_JEXEC') or die;
+// include the syndicate functions only once
+//require_once dirname(__FILE__).'/helper.php';
+?>
+<?php
+    echo $params->get('greeting');
+?>
