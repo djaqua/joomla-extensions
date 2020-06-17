51c16a2c47efd9c6adfb06c1c359eec174af73dd
Drew Jaqua <anjaqua@gmail.com>
2020-06-17 02:12:34 -0400

    (midna) added database capabilities to component

--- com_helloworld/site/models/helloworld.php
+++ com_helloworld/site/models/helloworld.php
@@ -22,31 +22,53 @@ class HelloWorldModelHelloWorld extends JModelItem
 	 */
 	protected $message;
 
+
+    /**
+	 * Method to get a table object, load it if necessary.
+	 *
+	 * @param   string  $type    The table name. Optional.
+	 * @param   string  $prefix  The class prefix. Optional.
+	 * @param   array   $config  Configuration array for model. Optional.
+	 *
+	 * @return  JTable  A JTable object
+	 *
+	 * @since   1.6
+	 */
+	public function getTable($type = 'HelloWorld', $prefix = 'HelloWorldTable', $config = array())
+	{
+		return JTable::getInstance($type, $prefix, $config);
+	}
+
 	/**
 	 * Get the message
-         *
-	 * @return  string  The message to be displayed to the user
+	 *
+	 * @param   integer  $id  Greeting Id
+	 *
+	 * @return  string        Fetched String from Table for relevant Id
 	 */
-	public function getMsg()
+	public function getMsg($id = 1)
 	{
-		if (!isset($this->message))
+		if (!is_array($this->messages))
 		{
-			// $this->message = 'Hello World! (defined in site/models/helloworld.php)';
-            $jinput = JFactory::getApplication()->input;
-			$id     = $jinput->get('id', 1, 'INT');
-
-			switch ($id)
-			{
-				case 2:
-					$this->message = 'Good bye World!';
-					break;
-				default:
-				case 1:
-					$this->message = 'Hello World!';
-					break;
-			}
+			$this->messages = array();
 		}
 
-		return $this->message . "(defined in site/models/helloworld.php)";
+		if (!isset($this->messages[$id]))
+		{
+			// Request the selected id
+			$jinput = JFactory::getApplication()->input;
+			$id     = $jinput->get('id', 1, 'INT');
+
+			// Get a TableHelloWorld instance
+			$table = $this->getTable();
+
+			// Load the message
+			$table->load($id);
+
+			// Assign the message
+			$this->messages[$id] = $table->a_column;
+		}
+
+		return $this->messages[$id] . "(defined in site/models/helloworld.php)";
 	}
 }
